745a47d98eecf42359b8e34160f4e6d94f0a70e4
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-01-19 01:20:32 +0200

    Update - Third task from the MID Exam

---
+++ Mid exam/Numbers/Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> numbers = Console.ReadLine().Split(" ").Select(double.Parse).ToList();
+            double average = Queryable.Average(numbers.AsQueryable());
+           List<double> topNumbers = numbers.Where(n => n > average).OrderByDescending(n => n).Take(5).ToList();
+            if (!topNumbers.Any())
+            {
+                Console.WriteLine("No");
+            }
+            else
+            Console.WriteLine(string.Join(" ",topNumbers));
+            
+            
+        }
+    }
+}
