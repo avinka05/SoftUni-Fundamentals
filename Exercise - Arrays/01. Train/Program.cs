88b7a7d3a51536cfc7ec8918e6deba1de84ce043
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-01-20 23:07:48 +0200

    Exercise - Arrays\01. Train\

---
+++ Exercise - Arrays/01. Train/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01._Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] rawPeople = new string[n];
+
+            int sum = 0;
+                       
+            for (int i = 0; i < n; i++)
+            {
+                rawPeople[i] = Console.ReadLine();
+                int people = int.Parse(rawPeople[i]);
+                sum+= people;
+            }
+            Console.Write(string.Join(" ", rawPeople));
+            Console.WriteLine();
+            Console.WriteLine(sum);
+        }
+    }
+}
