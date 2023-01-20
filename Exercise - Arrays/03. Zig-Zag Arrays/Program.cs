75ef50501530f2f0e0bd8d50fabda674b60389c5
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-01-20 23:10:22 +0200

    Exercise - Arrays\03. Zig-Zag Arrays\

---
+++ Exercise - Arrays/03. Zig-Zag Arrays/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Numerics;
+
+class NumberToAlphabet
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] numbers = Console.ReadLine().Split(' ');
+
+            long leftNumber = long.Parse(numbers[0]);
+            long rightNumber = long.Parse(numbers[1]);
+
+            
+            if (leftNumber > rightNumber)
+            {
+                int sum = 0;
+                foreach (char c in numbers[0])
+                {
+                    if (Char.IsDigit(c))
+                    {
+                        sum += (c - '0');
+                    }
+                }
+                Console.WriteLine(sum);
+            }
+            else if (leftNumber < rightNumber)
+            {
+                int sum = 0;
+                foreach (char c in numbers[1])
+                {
+                    if (Char.IsDigit(c))
+                    {
+                        sum += (c - '0');
+                    }
+                }
+                Console.WriteLine(sum);
+            }
+        }
+            
+    }
+}
