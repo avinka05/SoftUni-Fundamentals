edf302b1d5cdfe6a1a95b55516075fe9645b6a01
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-01-20 23:11:33 +0200

    Exercise - Arrays\04. Array Rotation\

---
+++ Exercise - Arrays/04. Array Rotation/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace _04._Array_Rotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int currentNum = arr[i];
+                bool isTopInteger = true;
+
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    int nextNum = arr[j];
+                    if (nextNum >= currentNum)
+                    {
+                        isTopInteger = false;
+                        break;
+                    }
+                }
+                if (isTopInteger)
+                {
+                    Console.Write($"{arr[i]} ");
+                }
+            }
+
+        }
+    }
+}
