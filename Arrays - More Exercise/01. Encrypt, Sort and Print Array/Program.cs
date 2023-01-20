4d1b5045308b6316a7dfac4c98459cbb83a5738f
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-01-20 22:52:55 +0200

    Arrays - More Exercise

---
+++ Arrays - More Exercise/01. Encrypt, Sort and Print Array/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Encrypt__Sort_and_Print_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] sortedArray = new int[n];           
+
+            for (int i = 0; i < n; i++)
+            {
+                char[] text = Console.ReadLine().ToArray();
+                char[] vowels = { 'A', 'a', 'E', 'e', 'U', 'u', 'I', 'i', 'O', 'o' };
+                int vowelsSum = text.Where(x => vowels.Contains(x)).Sum(x => x * text.Length);
+                int consonantSum = text.Where(x => !vowels.Contains(x)).Sum(x => x / text.Length);
+                sortedArray[i] = vowelsSum + consonantSum; 
+            }
+
+            //for (int i = 0; i < sortedArray.Length; i++)
+            //{
+            //    int[] currentSum = sortedArray.OrderBy(x => x).ToArray();
+            //    Console.WriteLine(currentSum[i]);
+            //}
+
+            Console.WriteLine(String.Join(Environment.NewLine, sortedArray.OrderBy(x => x)));
+        }
+    }
+}
---
+++ Arrays - More Exercise/02. Pascal Triangle/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Pascal_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            string input;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] tokens = input.Split(' ');
+
+                string command = tokens[0];
+                
+
+                if (command == "swap")
+                {
+                    int firstIndex = int.Parse(tokens[1]);
+                    int secondIndex = int.Parse(tokens[2]);
+
+                    Swap(list, firstIndex, secondIndex);
+                }
+                else if (command == "multiply")
+                {
+                    int firstIndex = int.Parse(tokens[1]);
+                    int secondIndex = int.Parse(tokens[2]);
+
+                    Multiply(list, firstIndex, secondIndex);
+                }
+                else if (command == "decrease")
+                {
+                    Decrease(list);
+                }
+            }
+
+            Console.WriteLine(String.Join(", ", list));
+        }
+
+        static List<int> Swap(List<int> list, int firstIndex, int secondIndex)
+        {
+            (list[firstIndex], list[secondIndex]) = (list[secondIndex], list[firstIndex]);
+            return list;
+            //int element = list[firstIndex];
+            //list[firstIndex] = list[secondIndex];
+            //list[secondIndex] = element;
+            //return list;
+        }
+        static List<int> Multiply(List<int> list, int firstIndex, int secondIndex)
+        {
+            list[firstIndex] = list[firstIndex] * list[secondIndex];
+
+            return list;            
+        }
+        static List<int> Decrease(List<int> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                list[i]--;
+            }
+            return list;
+        }
+        
+    }
+}
