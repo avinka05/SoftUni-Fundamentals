564f8f98485770024396f89d21e781b2562d799c
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-01-23 22:10:57 +0200

    Exercise - Lists\01

---
+++ Exercise - Lists/01. Train/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagons = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            int capacity = int.Parse(Console.ReadLine());
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                string[] tokens = command.Split(' ');
+                string action = tokens[0];
+
+                if (action == "Add")
+                {
+                    int addPassengers = int.Parse(tokens[1]);
+                    wagons.Add(addPassengers);
+                }
+                else
+                {
+                    int passengers = int.Parse(tokens[0]);
+                    for (int i = 0; i < wagons.Count; i++)
+                    {
+                        if (wagons[i] + passengers <= capacity)
+                        {
+                            wagons[i] += passengers;
+                            break;
+                        }
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" ", wagons));
+        }
+    }
+}      
+    
+
