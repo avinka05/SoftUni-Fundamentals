c01b40969f2a7092014cc5191be1191276740c40
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-01-23 22:12:57 +0200

    Exercise - Lists\05.

---
+++ Exercise - Lists/05. Bomb Numbers/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Bomb_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            int[] bombInfo = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int bombNumber = bombInfo[0];
+            int bombPower = bombInfo[1];
+
+            while (true)
+            {
+                int indexOfBomb = numbers.IndexOf(bombNumber);
+
+                if (indexOfBomb == -1)
+                {
+                    break;
+                }
+                DetonateBomb(numbers, indexOfBomb, bombPower);
+            }
+            Console.WriteLine(numbers.Sum());
+        }
+
+        static void DetonateBomb(List<int>numbers, int indexOfBomb, int bombPower)
+        {
+            int rightCount = indexOfBomb + bombPower;
+
+            for (int count = indexOfBomb; count <= rightCount; count++)
+            {
+                if (indexOfBomb >= numbers.Count)
+                {
+                    break;
+                }
+                numbers.RemoveAt(indexOfBomb);
+            }
+
+            int leftCount = indexOfBomb - bombPower;
+
+            if (leftCount < 0)
+            {
+                leftCount = 0;
+            }
+
+            for (int count = leftCount; count < indexOfBomb; count++)
+            {
+                numbers.RemoveAt(leftCount);
+            }
+        }
+    }
+}
