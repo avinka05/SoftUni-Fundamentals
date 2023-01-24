5d95d3f793a4f85d9865aa9e14ac5a799fd3a495
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-01-24 23:08:23 +0200

    Exercise - Lists\10.

---
+++ Exercise - Lists/10. SoftUni Course Planning/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10._SoftUni_Course_Planning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
