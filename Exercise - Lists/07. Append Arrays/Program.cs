ca0d03b0356e8720f4d166219146b309bbde7876
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-01-24 23:07:29 +0200

    Exercise - Lists\07.

---
+++ Exercise - Lists/07. Append Arrays/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _07._Append_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split('|').ToArray();
+            List<int> result = new List<int>();
+
+            //BackRow iteration 
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                //adds all numbers backwards in the "result List".
+                List<int> trimmedNums = input[i]
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToList();
+                result.AddRange(trimmedNums);
+            }
+            //Prints the result List
+            Console.WriteLine(String.Join(' ',result));
+            
+
+        }
+    }
+}
