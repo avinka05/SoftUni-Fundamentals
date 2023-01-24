d27a7d946b770b2a1a70da5bd45b07c16dbaabd2
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-01-24 23:08:06 +0200

    Exercise - Lists\09.

---
+++ Exercise - Lists/09. Pokemon Don't Go/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09._Pokemon_Don_t_Go
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] secondArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int index = firstArray.Zip(secondArray, (x, y) => x == y).ToList().IndexOf(false);
+
+            if (index >= 0)
+            {
+                Console.WriteLine($"Arrays are not equal. Found difference at {index} index ");
+            }
+            else
+            {
+                Console.WriteLine($"Arrays are equal.");
+            }
+        }
+    }
+}
