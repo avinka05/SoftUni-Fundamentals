4f067efc36702bcaa5cb373c9f5825af1f1e8170
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-01-25 23:22:11 +0200

    Methods - Lab\01.

---
+++ Methods - Lab/01. Sign of Integer Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _01._Sign_of_Integer_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+            SignOfNumbers(numbers);
+        }
+
+        static void SignOfNumbers(int number)
+        {
+            if (number > 0)
+            {
+                Console.WriteLine($"The number {number} is positive.");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine($"The number {number} is negative.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {number} is zero.");
+            }
+        }
+    }
+}
