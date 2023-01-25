b6aeb60b672af3ec3183c1fdfc5cac48bba518a9
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-01-25 23:26:52 +0200

    Objects and Classes - Lab\03.

---
+++ Objects and Classes - Lab/03. Songs/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace _03._Songs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List <Song> songs = new List<Song>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] token = Console.ReadLine().Split("_");
+                //favourite_DownTown_3:14 ->
+                //token[0] - typeList | token[1] - name | token[2] - time 
+
+                string typeList = token[0];
+                string name = token[1];
+                string time = token[2];
+
+                Song song = new Song (typeList, name, time);
+                songs.Add (song);
+            }
+
+            string command = Console.ReadLine();
+
+            for (int i = 0; i < songs.Count; i++)
+            {
+                Song currentSong = songs[i];
+                if (command == "all")
+                {
+                    Console.WriteLine(currentSong.Name);
+                }
+                if (command == currentSong.TypeList)
+                {
+                    Console.WriteLine(currentSong.Name);
+                }
+            }
+            
+        }
+    }
+
+
+}
---
+++ Objects and Classes - Lab/03. Songs/Songs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03._Songs
+{
+    public class Song
+    {
+        //Constructor Create
+        public Song(string typeList, string name, string time)
+        {
+            TypeList = typeList;
+            Name = name;
+            Time = time;
+        }
+
+        //-------------------------------
+
+        //Properties of Song Class
+        public string TypeList { get; set; }
+
+        public string Name { get; set; }
+
+        public string Time { get; set; }
+    }
+
+
+}
