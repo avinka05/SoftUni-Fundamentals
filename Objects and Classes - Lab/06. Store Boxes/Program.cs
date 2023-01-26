f76d15c70bbac4f2f99784096d9c4c57a6864b7a
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-01-26 20:08:45 +0200

    Objects and Classes - Lab\06.

---
+++ Objects and Classes - Lab/06. Store Boxes/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Store_Boxes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Box> boxes = new List<Box>();
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                string[] tokens = input.Split(' ');
+                //"{Serial Number} {Item Name} {Item Quantity} {itemPrice}"
+
+                string serialNumber = tokens[0];
+                string itemName = tokens[1];
+                int itemQuantity = int.Parse(tokens[2]);
+                decimal itemPrice = decimal.Parse(tokens[3]);
+
+                Item item = new Item(itemName, itemPrice);
+                Box box = new Box(serialNumber, item, itemQuantity);
+                boxes.Add(box);
+                input = Console.ReadLine();
+            }
+
+            foreach (Box box in boxes.OrderByDescending(x => x.BoxPrice))
+            {
+                Console.WriteLine($"{box.SerialNumber}");
+                Console.WriteLine($"-- {box.Item.Name} - ${box.Item.ItemPrice:f2}: {box.ItemQuantity}");
+                Console.WriteLine($"-- ${box.BoxPrice:f2}");
+            }
+        }
+    }
+    public class Item
+    {
+        public string Name { get; set; }
+
+        public decimal ItemPrice { get; set; }
+
+        public Item(string name, decimal itemPrice)
+        {
+            Name = name;
+            ItemPrice = itemPrice;
+        }
+    }
+    public class Box
+    {
+        public string SerialNumber { get; set; }
+
+        public Item Item { get; set; }
+
+        public int ItemQuantity { get; set; }
+
+        public decimal BoxPrice
+        {
+            get
+            {
+                return ItemQuantity * Item.ItemPrice;
+            }
+        }
+
+        public Box(string serialNumber, Item item, int itemQuantity)
+        {
+            SerialNumber = serialNumber;
+            Item = item;
+            ItemQuantity = itemQuantity;
+        }
+    }
+}
