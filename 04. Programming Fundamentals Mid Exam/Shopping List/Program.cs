c0b38e4da9bd2a55029b77dc08ca74957ae449c4
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-02-07 22:17:45 +0200

    Mid Exam

---
+++ 04. Programming Fundamentals Mid Exam/Shopping List/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shopping_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> shopingList = Console.ReadLine().Split("!").ToList();
+            string input = " ";
+
+            while ((input=Console.ReadLine())!= "Go Shopping!")
+            {
+              
+                List<string> comand = input.Split(" ").ToList();
+
+                if (comand[0]== "Urgent")
+                {
+                    comand[1] = shopingList[0];
+                }
+                else if (comand[0] == "Unnecessary")
+                {
+                    shopingList.Remove(comand[1]);
+                }
+                else if (comand[0] == "Correct")
+                {
+                    if (shopingList.Contains(comand[1]))
+                    {
+                        int index = shopingList.FindIndex(i => i == comand[1]);
+                           if (index!=-1)
+                           {
+                                 shopingList.Insert(index, comand[2]);
+                                 shopingList.RemoveAt(index);
+                           }
+                    }
+                        
+                                                          
+                }
+                else if (comand[0] == "Rearrange")
+                {
+                    if (shopingList.Contains(comand[1]))
+                    {
+                        shopingList.Remove(comand[1]);
+                        shopingList.Add(comand[1]);
+                    }
+                    
+                }
+            }
+            Console.WriteLine(string.Join(", ",shopingList));
+        }
+    }
+}
