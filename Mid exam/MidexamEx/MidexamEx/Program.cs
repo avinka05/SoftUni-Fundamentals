a0c3f56809af884648a08054b9b04cecc70b5fd6
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-02-09 21:53:54 +0200

    1

---
+++ Mid exam/MidexamEx/MidexamEx/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace MidexamEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using System;
+            using System.Collections.Generic;
+            using System.Linq;
+
+
+namespace _02._Shopping_List
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                List<string> list = Console.ReadLine().Split("!").ToList();
+
+                string input;
+
+                while ((input = Console.ReadLine()) != "Go Shopping!")
+                {
+                    string[] command = input.Split(' ');
+                    string action = command[0];
+
+                    if (action == "Urgent")
+                    {
+                        string item = command[1];
+                        Urgent(list, item);
+                    }
+                    else if (action == "Unnecessary")
+                    {
+                        string item = command[1];
+                        Unnecessary(list, item);
+                    }
+                    else if (action == "Correct")
+                    {
+                        string oldItem = command[1];
+                        string newItem = command[2];
+                        Correct(list, oldItem, newItem);
+                    }
+                    else if (action == "Rearrange")
+                    {
+                        string item = command[1];
+                        Rearrange(list, item);
+                    }
+                }
+
+        }
+    }
+
+}
+    }
+}
