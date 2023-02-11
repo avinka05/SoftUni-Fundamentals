4a3e8bada6d767afba0f4561101b62a2258a1264
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-02-11 23:33:34 +0200

    02.Lift

---
+++ Mid exam/01.MidExam/02.Lift/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._The_Lift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            int[] wagons = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            for (int i = 0; i < wagons.Length; i++)
+            {
+                while (wagons[i] < 4)
+                {
+                    if (people <= 0)
+                    {
+                        break;
+                    }
+                    wagons[i]++;
+                    people--;
+                }
+            }
+            if (people > 0)
+            {
+                Console.WriteLine($"There isn't enough space! {people} people in a queue!");
+                Console.WriteLine(String.Join(' ', wagons));
+            }
+            else
+            {
+                Console.WriteLine("The lift has empty spots!");
+                Console.WriteLine(String.Join(' ', wagons));
+            }
+
+        }
+    }
+}
+
