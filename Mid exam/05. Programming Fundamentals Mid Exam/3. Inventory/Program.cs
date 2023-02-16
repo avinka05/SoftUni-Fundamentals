9a5f5ee19df3a25a3732cac310a32f0f3f0e3d86
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-02-16 22:27:57 +0200

    3. Inventory

---
+++ Mid exam/05. Programming Fundamentals Mid Exam/3. Inventory/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Inventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> items = Console.ReadLine().Split(", ").ToList();
+            string input = Console.ReadLine();
+            while (input!= "Craft!")
+            {
+                string[] command = input.Split(" ");
+
+                string commandName = command[0];
+
+                if (commandName=="Collect" )
+                {
+                    string item = command[2];
+                    if (items.Contains(item))
+                    {
+                        input = Console.ReadLine();
+                        continue;
+                    }
+                    else
+                    {
+                        items.Add(item);
+                    }
+
+                }
+
+                else if (commandName == "Drop")
+                {
+                    string item = command[2];
+                    if (!items.Contains(item))
+                    {
+                        input = Console.ReadLine();
+                        continue;
+                    }
+                    else
+                    {
+                        items.Remove(item);
+                    }
+                }
+
+                else if (commandName == "Combine")
+                {
+                    string[] split = command[3].Split(":");
+                    string oldItem = split[0];
+                    string newItem = split[1];
+
+                    if (!items.Contains(oldItem))
+                    {
+                        input = Console.ReadLine();
+                        continue;
+                    }
+                    else
+                    {
+                        int index = items.IndexOf(oldItem);
+                        index++;
+                        items.Insert(index, newItem);
+                    }
+
+                }
+
+                else if (commandName == "Renew")
+                {
+                    string item = command[2];
+                    if (items.Contains(item))
+                    {
+                        items.Remove(item);
+                        items.Add(item);
+                    }
+
+                }
+                input = Console.ReadLine();
+
+            }
+
+            Console.WriteLine(string.Join(", ",items));
+        }
+    }
+}
