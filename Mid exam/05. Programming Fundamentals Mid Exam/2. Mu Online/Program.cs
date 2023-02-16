fd049b54bc07b2179e663010aa29544469efd3f0
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-02-16 22:27:43 +0200

    2. Mu Online

---
+++ Mid exam/05. Programming Fundamentals Mid Exam/2. Mu Online/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace _2._Mu_Online
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split("|");
+            int health = 100;
+            int bicoins = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string[] command = input[i].Split(" ");
+
+                string commandName = command[0];
+                int number = int.Parse(command[1]);
+
+                if (commandName == "potion")
+                {
+                    int curentHealt = health;
+                    health += number;
+                    if (health>100)
+                    {
+                        health = 100;
+                        number = 100 - curentHealt;
+                    }
+                    
+                    Console.WriteLine($"You healed for {number} hp.");
+                    Console.WriteLine($"Current health: {health} hp.");
+                }
+                else if (commandName == "chest")
+                {
+                    Console.WriteLine($"You found {number} bitcoins.");
+                    bicoins += number;
+                }
+                else
+                {
+                    health -= number;
+                    if (health > 0)
+                    {
+                        Console.WriteLine($"You slayed {commandName}.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You died! Killed by {commandName}.");
+                        Console.WriteLine($"Best room: {i + 1}");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine("You've made it!");
+            Console.WriteLine($"Bitcoins: {bicoins}");
+            Console.WriteLine($"Health: {health}");
+
+
+        }
+    }
+}
+
