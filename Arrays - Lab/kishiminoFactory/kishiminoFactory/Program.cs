975119667cd59bfc2b1d141d4d4df0e66d03c06a
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-02-18 19:50:41 +0200

    Update Program.cs

--- Arrays - Lab/kishiminoFactory/kishiminoFactory/Program.cs
+++ Arrays - Lab/kishiminoFactory/kishiminoFactory/Program.cs
@@ -86,13 +86,12 @@ namespace KaminoFactory
                             lss = data;
                             lssStart = index;
                         }
-                    }s
+                    }
                 }
                 index++;
             }
 
-            Console.WriteLine($"Best DNA sample {lssStart} with sum: {lssSum}.");
-            Console.WriteLine(string.Join(" ", lss));
+            
         }
     }
 }
