ca1b17fa3e2b3d07825a47507113fa1ff656b0bd
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-02-19 13:20:51 +0200

    01._Experience_Gaining

---
+++ Mid exam/01._Experience_Gaining/01._Experience_Gaining/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _01._Experience_Gaining
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double expNeeded = double.Parse(Console.ReadLine());
+            int battles = int.Parse(Console.ReadLine());
+
+            double totalExp = 0;
+            int count = 0;
+
+            for (int i = 1; i <= battles; i++)
+            {
+                double currentExp = double.Parse(Console.ReadLine());
+                count++;
+                totalExp += currentExp;
+
+                if (count % 3 == 0)
+                {
+                    totalExp += currentExp * 0.15;
+                }
+                if (count % 5 == 0)
+                {
+                    totalExp -= currentExp * 0.10;
+                }
+                if (count % 15 == 0)
+                {
+                    totalExp += currentExp * 0.05;
+                }
+                if (totalExp >= expNeeded)
+                {
+                    break;
+                }
+            }
+
+            if (totalExp >= expNeeded)
+            {
+                Console.WriteLine($"Player successfully collected his needed experience for {count} battles.");
+            }
+            else
+            {
+                Console.WriteLine($"Player was not able to collect the needed experience, {expNeeded - totalExp:f2} more needed.");
+            }
+        }
+    }
+}
---
+++ Mid exam/01._Experience_Gaining/02.Coffee Lover/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Coffee_Lover
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> coffee = Console.ReadLine().Split().ToList();
+            int numberOfCommands = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                string command = Console.ReadLine();
+                string[] tokens = command.Split();
+                string action = tokens[0];
+
+                switch (action)
+                {
+                    case "Include":
+                        string coffeeToInclude = tokens[1];
+                        coffee.Add(coffeeToInclude);
+                        break;
+
+                    case "Remove":
+                        int numberOfCoffees = int.Parse(tokens[2]);
+                        if (coffee.Count < numberOfCoffees)
+                        {
+                            break;
+                        }
+
+                        if (tokens[1] == "first")
+                        {
+                            for (int j = 0; j < numberOfCoffees; j++)
+                            {
+                                coffee.RemoveAt(0);
+                            }
+                        }
+
+                        if (tokens[1] == "last")
+                        {
+                            for (int j = 0; j < numberOfCoffees; j++)
+                            {
+                                coffee.RemoveAt(coffee.Count - 1);
+                            }
+                        }
+                        break;
+                    case "Prefer":
+                        int coffeeIndex = int.Parse(tokens[1]);
+                        int coffeeIndex2 = int.Parse(tokens[2]);
+
+                        if (coffeeIndex >= 0 && coffeeIndex <= coffee.Count - 1 && coffeeIndex2 >= 0 && coffeeIndex2 <= coffee.Count - 1)
+                        {
+                            Swap(coffeeIndex, coffeeIndex2, coffee);
+
+                        }
+                        break;
+                    case "Reverse":
+                        coffee.Reverse();
+                        break;
+
+                }
+            }
+            Console.WriteLine("Coffees:");
+            Console.WriteLine(string.Join(' ', coffee));
+
+
+        }
+       
+    }
+
+}
+
