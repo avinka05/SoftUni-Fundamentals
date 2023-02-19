9c7d8020120f9b7a5237f954a7dea9f1a1fef623
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-02-19 13:25:39 +0200

    Update Program.cs

--- Mid exam/01._Experience_Gaining/02.Coffee Lover/Program.cs
+++ Mid exam/01._Experience_Gaining/02.Coffee Lover/Program.cs
@@ -17,7 +17,7 @@ namespace _02._Coffee_Lover
                 string[] tokens = command.Split();
                 string action = tokens[0];
 
-                switch (true)
+                switch (action)
                 {
                     case "Include":
                         string coffeeToInclude = tokens[1];
