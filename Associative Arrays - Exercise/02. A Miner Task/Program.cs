d46ec9501790694ff22248d79ffaedeae200fdcd
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-03-05 22:03:22 +0200

    02. A Miner Task

---
+++ Associative Arrays - Exercise/02. A Miner Task/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Dictionary<string, double> resources = new Dictionary<string, double>();
+
+            while (input!="stop")
+            {
+                double quantity = double.Parse(Console.ReadLine());
+
+                if (!resources.ContainsKey(input))
+                {
+                    resources[input] = 0.0;
+                }
+                resources[input] += quantity;
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var item in resources)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
