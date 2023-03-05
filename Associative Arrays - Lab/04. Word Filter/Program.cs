01492e5e09be71d5417f1c7a7f6a0d0125e30544
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-03-05 10:42:17 +0200

    04. Word Filter

---
+++ Associative Arrays - Lab/04. Word Filter/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Word_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine().Split(" ").Where(x => x.Length % 2 == 0).ToList().ForEach(Console.WriteLine);    
+        }
+    }
+}
