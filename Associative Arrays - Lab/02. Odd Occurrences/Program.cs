fb72751a104598fc55f309c658c3ca90cb7a9ddc
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-03-05 10:43:01 +0200

    02. Odd Occurrences

---
+++ Associative Arrays - Lab/02. Odd Occurrences/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Odd_Occurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(" ");
+
+            Dictionary<string, int> counts = new Dictionary<string, int>();
+            foreach (var word in words)
+            {
+                string currentWord = word.ToLower();
+                if (!counts.ContainsKey(currentWord))
+                {
+                    counts.Add(currentWord, 0);
+
+                }
+                counts[currentWord]++;
+
+            }
+            foreach (var item in counts)
+            {
+                if (item.Value%2 != 0)
+                {
+                    Console.Write(item.Key + " ");
+                }
+            }
+
+        }
+    }
+}
