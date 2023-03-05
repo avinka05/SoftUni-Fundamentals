ba3de54e0a581688fbf942f2e25ebf7c534bd6e1
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-03-05 10:42:45 +0200

    03. Word Synonyms

---
+++ Associative Arrays - Lab/03. Word Synonyms/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace _03._Word_Synonyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num =int.Parse(Console.ReadLine());
+
+            var words = new Dictionary<string, List<string>>(); 
+
+            for (int i = 0; i < num; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+                if (!words.ContainsKey(word))
+                {
+                    words.Add(word, new List<string>());
+                }
+
+
+                words[word].Add(synonym);
+            }
+            foreach (var (key,value) in words)
+            {
+                Console.WriteLine($"{key} - {String.Join(", ",value)}");
+            }
+        }
+    }
+}
