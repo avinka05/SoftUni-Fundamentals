79da3e43a9f2a74066d0cc4a2c1c3627637b1abd
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-03-09 20:50:57 +0200

    Update Program.cs

--- Associative Arrays - Exercise/04. SoftUni Parking/Program.cs
+++ Associative Arrays - Exercise/04. SoftUni Parking/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _04._SoftUni_Parking
 {
@@ -6,7 +8,53 @@ namespace _04._SoftUni_Parking
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int number = int.Parse(Console.ReadLine());
+            var map = new Dictionary<string, string>();
+
+            for (int i = 0; i < number; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ");
+                string command = input[0];
+                string username = input[1];
+
+                if (command== "register")
+                {
+                    
+                    string licensePlateNumber = input[2];
+
+                    if (!map.ContainsKey(username))
+                    {
+                        map[username] = licensePlateNumber;
+                        Console.WriteLine($"{username} registered {licensePlateNumber} successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {licensePlateNumber}");
+                    }
+                    
+                }
+                else
+                {
+                    if (!map.ContainsKey(username))
+                    {
+                        Console.WriteLine($"ERROR: user {username} not found");
+                    }
+                    else
+                    {
+                        map.Remove(username);
+
+                        Console.WriteLine($"{username} unregistered successfully");
+                    }
+                                
+                }
+            }
+
+
+            foreach (var item in map)
+            {
+                Console.WriteLine($"{item.Key} => {item.Value}");
+            }
+
         }
     }
 }
