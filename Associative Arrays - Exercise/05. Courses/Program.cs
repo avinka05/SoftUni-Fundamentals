f47f6dc77dcd196decc75bb1b32838120e307aad
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-03-09 20:51:18 +0200

    05. Courses

---
+++ Associative Arrays - Exercise/05. Courses/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace _05._Courses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var map = new Dictionary<string, List<string>>();
+            string input = string.Empty;
+            while ((input=Console.ReadLine())!="end")
+            {
+
+                string[] words = input.Split(" : ");
+                string courseName = words[0];
+                string registeredStudents = words[1];
+
+                if (!map.ContainsKey(courseName))
+                {
+                    map[courseName] = new List<string>();
+                }
+                map[courseName].Add(registeredStudents);
+
+            }
+
+            foreach (var item in map)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value.Count()}");
+
+                foreach (var student in item.Value)
+                {
+                    Console.WriteLine($"-- {student}");
+                }
+                
+            }
+        }
+    }
+}
