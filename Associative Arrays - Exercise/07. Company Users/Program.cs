97573d27efa760127afc5388e065404f6d323e0b
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-03-11 21:28:51 +0200

    Update Program.cs

--- Associative Arrays - Exercise/07. Company Users/Program.cs
+++ Associative Arrays - Exercise/07. Company Users/Program.cs
@@ -24,6 +24,7 @@ namespace _07._Company_Users
                     map[company] = new List<string>();
                 }
 
+
                 if (!map[company].Contains(employeeId))
                 {
                     map[company].Add(employeeId);
