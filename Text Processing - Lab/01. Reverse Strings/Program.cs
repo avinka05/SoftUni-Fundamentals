c7b6b568e677effecbe958dcfdcbda6c6df9aea0
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-03-12 18:09:51 +0200

    01. Reverse Strings

---
+++ Text Processing - Lab/01. Reverse Strings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace _01._Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+
+            while ((input=Console.ReadLine())!="end")
+            {
+                StringBuilder reversedWord=new StringBuilder();
+                for (int i = input.Length-1; i >= 0; i--)
+                {
+                    reversedWord.Append(input[i]);
+                }
+                string result = reversedWord.ToString();
+
+                Console.WriteLine($"{input} = {result}");
+            }
+        }
+    }
+}
