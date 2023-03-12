531842b9d7a2355d773ba07b646baeef8703f802
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-03-12 18:10:36 +0200

    03. Substring

---
+++ Text Processing - Lab/03. Substring/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03._Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string wordToRemove = Console.ReadLine();
+            string words = Console.ReadLine();
+
+            while (words.Contains(wordToRemove))
+            {
+                words = words.Replace(wordToRemove, "");
+            }
+
+            Console.WriteLine(words);
+        }
+    }
+}
