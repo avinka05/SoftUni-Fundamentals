753b713cd9d6d9b6a76881c04624f2eb36b67eda
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-03-12 18:10:53 +0200

    04. Text Filter

---
+++ Text Processing - Lab/04. Text Filter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _04._Text_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] wordsToRemove = Console.ReadLine().Split(", ");
+            string text = Console.ReadLine();
+
+            foreach (var word in wordsToRemove)
+            {                
+                    string substitution = new string('*', word.Length);
+                    text = text.Replace(word, substitution);               
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
