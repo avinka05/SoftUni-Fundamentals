9dfe0f9ab2d389b57a68221d40e4446ee68f03b5
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-03-12 18:11:11 +0200

    05. Digits, Letters and Other

---
+++ Text Processing - Lab/05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Digits__Letters_and_Other
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            Console.WriteLine(string.Join("",text.Where(char.IsDigit)));
+            Console.WriteLine(string.Join("", text.Where(char.IsLetter)));
+            Console.WriteLine(string.Join("", text.Where(x=> !char.IsDigit(x) && !char.IsLetter(x))));
+        }
+    }
+}
