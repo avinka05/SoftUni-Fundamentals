bf4a882266706952505d649d265e2647df7a646d
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-03-12 18:10:24 +0200

    02. Repeat Strings\Program.cs

---
+++ Text Processing - Lab/02. Repeat Strings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using System;
+
+namespace _02._Repeat_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine().Split();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var word in array)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    sb.Append(word);
+                }
+            }
+
+            string result = sb.ToString();
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
