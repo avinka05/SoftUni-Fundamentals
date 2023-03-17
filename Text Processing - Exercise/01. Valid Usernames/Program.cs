9d63dcf5e8ae1681a0041c86972d769694a99f00
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-03-17 21:43:24 +0200

    Update Program.cs

--- Text Processing - Exercise/01. Valid Usernames/Program.cs
+++ Text Processing - Exercise/01. Valid Usernames/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Linq;
 
-namespace _01._Valid_Usernames
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.Write("Enter user names separated by commas: ");
+        string input = Console.ReadLine();
+        string[] usernames = input.Split(',')
+                                   .Select(username => username.Trim())
+                                   .Where(username => username.Length >= 3 && username.Length <= 16 &&
+                                                      username.All(c => char.IsLetterOrDigit(c) || c == '-' || c == '_'))
+                                   .ToArray();
+
+        Console.WriteLine("Valid user names:");
+        foreach (string username in usernames)
         {
-            Console.WriteLine("Hello World!");
-        }
+            Console.WriteLine(username);
+        } 
     }
 }
