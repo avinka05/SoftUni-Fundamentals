f4c1765d53488656904685c4fead82f2567258d7
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-03-19 19:16:46 +0200

    03. Match Dates

---
+++ Regular Expressions - Lab/03. Match Dates/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03._Match_Dates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string pattern = @"\b(?<day>\d{2})(\.|-|/)(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b";
+
+            var matches = Regex.Matches(text, pattern);
+
+            foreach (Match item in matches)
+            {
+                var groups = item.Groups;
+                Console.WriteLine($"Day: {groups["day"]}, Month: {groups["month"]}, Year: {groups["year"]}");
+            }
+        }
+    }
+}
