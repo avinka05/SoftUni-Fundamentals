160569dbb7e0906c01bdba35de85b576cd08b4c2
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-03-19 19:15:26 +0200

    01. Match Full Name

---
+++ Regular Expressions - Lab/01. Match Full Name/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+namespace _01._Match_Full_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            var name = (@"\b[A-Z][a-z]+ [A-Z][a-z]+");
+            var matches = Regex.Matches(input,name);
+
+            Console.WriteLine(string.Join(" ",matches));
+        }
+    }
+}
