f7eb2bfe0bf3213712664e8c4f648eba8fb278e0
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-03-19 19:16:26 +0200

    02. Match Phone Number

---
+++ Regular Expressions - Lab/02. Match Phone Number/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+namespace _01._Match_Full_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            var pattern = @"\+359(-| )2\1\d{3}\1\d{4}\b";
+
+            var matches = Regex.Matches(input, pattern);
+
+
+
+            Console.WriteLine(string.Join(", ", matches));
+        }
+    }
+}
