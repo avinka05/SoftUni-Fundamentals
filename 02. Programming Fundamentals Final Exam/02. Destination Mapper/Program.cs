204731136827cd18c09cb3d6b409c6f6b46c1d46
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-04-09 04:25:42 +0300

    Create Program.cs

---
+++ 02. Programming Fundamentals Final Exam/02. Destination Mapper/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _02._Destination_Mapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"(\=|\/)([A-Z][A-Za-z]{2,})\1";
+            var places = new List<string>();
+            int points = 0;
+
+            MatchCollection matchCollection = Regex.Matches(input,pattern);
+            foreach (Match place in matchCollection)
+            {
+                places.Add(place.ToString().Trim('=', '/'));
+            }
+
+            foreach (var item in places)
+            {
+                points += item.Length;
+            }
+
+
+            Console.WriteLine($"Destinations: {string.Join(", ",places)}");
+            Console.WriteLine($"Travel Points: {points}");
+
+        }
+    }
+}
