f437ba7dd143c2f889e9c6020c1fea937f9c46f7
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-04-15 17:52:50 +0300

    Create Program.cs

---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/01. Ages/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            string[] array = { "baby", "child", "teenager", "adult", "elder" };
+            string word = "";
+            int index = 0;
+            if (age < 3)
+            {
+
+            }
+            else if (age < 14)
+            {
+                index++;
+            }
+            else if (age < 20)
+            {
+                index = 2;
+            }
+            else if (age < 66)
+            {
+                index = 3;
+            }
+            else
+            {
+                index = 4;
+
+            }
+            Console.WriteLine($"{array[index]}");
+        }
+    }
+}
+
