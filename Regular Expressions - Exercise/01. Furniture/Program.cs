6f313326fdfa16ca1d1b0e27b6ca0a28a526ac25
avinka05 <122906792+avinka05@users.noreply.github.com>
2023-05-15 23:02:38 +0300

    Update Program.cs

--- Regular Expressions - Exercise/01. Furniture/Program.cs
+++ Regular Expressions - Exercise/01. Furniture/Program.cs
@@ -31,7 +31,7 @@ namespace _01._Furniture
             }
             Console.WriteLine($"Bought furniture:");
             Console.WriteLine($"{string.Join(Environment.NewLine, items)}");
-            Console.WriteLine($"Total money spend: {totalPrice:F2}");
+            Console.WriteLine($"Total money spend: {totalPrice:F2}")
         }
     }
 }
